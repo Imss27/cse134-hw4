ced0ff76bd269fe29ac3c29c121a589423087a63
Imss27 <1529767689@qq.com>
2023-11-29 21:33:31 -0800

    bug fixed

--- js/form.js
+++ js/form.js
@@ -1,40 +1,98 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contactForm');
-    const email = document.getElementById('email');
+window.addEventListener('DOMContentLoaded', () => {
+    let name_error = document.querySelector('output[name="nameError"]');
+    let name_info = document.querySelector('output[name="nameInfo"]');
+    let email_error = document.querySelector('output[name="emailError"]');
+    let email_info = document.querySelector('output[name="emailInfo"]');
+    let comments_error = document.querySelector('output[name="commentsError"]');
+    let comments_info = document.querySelector('output[name="commentsInfo"]');
+
     const comments = document.getElementById('comments');
-    let formErrors = []; 
 
-
-    form.addEventListener('submit', (event) => {
-        if (!form.checkValidity()) {
-            event.preventDefault();
-
-            const formErrorsJson = JSON.stringify(formErrors);
-
-            const hiddenInput = document.createElement('input');
-            hiddenInput.type = 'hidden';
-            hiddenInput.name = 'form-errors';
-            hiddenInput.value = formErrorsJson;
-
-  
-            form.appendChild(hiddenInput);
-            form.submit();
+    comments.addEventListener('input', () => {
+        const maxLength = comments.getAttribute('maxlength');
+        const currentLength = comments.value.length;
+        const infoOutput = document.querySelector('output[name="commentsInfo"]');
+        infoOutput.textContent = `${maxLength - currentLength} characters remaining`;
+        if (currentLength > maxLength - 10) {
+            infoOutput.classList.add('warning');
+        } else {
+            infoOutput.classList.remove('warning');
+        }
+        if (currentLength === maxLength) {
+            infoOutput.classList.add('error-message');
+        } else {
+            infoOutput.classList.remove('error-message');
         }
     });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    const modeChange = document.getElementById("mode");
-    const theme = localStorage.getItem("theme");
-    if (theme) {
-        document.body.classList.add(theme);
-        modeChange.textContent = theme === "dark" ? "mode 1" : "mode 2";
+    const name_exp = new RegExp('[a-zA-Z\- ]');
+    const email_exp = new RegExp('[a-zA-Z0-9\-_.@]');
+    const comment_exp = new RegExp('[a-zA-Z\- .!?\n,]');
+
+    let form_errors = document.querySelector('input[name="form-errors"]');
+
+    let form_errors_array = [];
+
+    let inputs = document.querySelectorAll('input');
+    let text_area = document.querySelector('textarea');
+    text_area.addEventListener('input', check_input);
+    for(let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('input', check_input);
     }
-    modeChange.onclick = () => {
-        document.body.classList.toggle("dark");
-        const updatedTheme = document.body.classList.contains("dark") ? "dark" : "light";
-        localStorage.setItem("theme", updatedTheme);
-        modeChange.textContent = updatedTheme === "dark" ? "mode 1" : "mode 2";
-    };
+    
+    document.querySelector('form').addEventListener('submit', form_submit);
+
+    function form_submit(event) {
+        console.log(event);
+    }
+
+    function check_input(event) {
+        let id = event.target.id;
+        if(event.target.value.length > 0) {           
+            if(id === 'name') {
+                if(name_exp.test(event.target.value.charAt(event.target.value.length - 1)) === false) {
+                    name_error.innerHTML = `${event.target.value.charAt(event.target.value.length -1)} is not allowed！`;
+                    append_error('invalid input');
+                    input_error(event);
+                    fade(name_error);
+                }
+            } 
+            if (id === 'email') {
+                if(email_exp.test(event.target.value.charAt(event.target.value.length - 1)) === false) {
+                    email_error.innerHTML = `${event.target.value.charAt(event.target.value.length -1)} is not allowed`;
+                    append_error('invalid input');
+                    input_error(event);
+                    fade(email_error);
+                }
+            }
+            if (id === 'comments') {
+                if(comment_exp.test(event.target.value.charAt(event.target.value.length - 1)) === false) {
+                    comments_error.innerHTML = `${event.target.value.charAt(event.target.value.length -1)} is not allowed`;
+                    append_error('invalid input');
+                    fade(comments_error);
+                    input_error(event);
+                }
+            }   
+        }
+    }
+
+    async function input_error(event) {
+        event.target.classList.add('invalid');
+        await new Promise(x => setTimeout(x, 400));
+        event.target.classList.remove('invalid');
+        
+    }
+
+    async function fade(element) {
+        element.classList.add('error-message');
+        await new Promise(x => setTimeout(x, 2000));
+        element.classList.remove('error-message');
+        element.innerHTML = '';
+    }
+
+    function append_error(string) {
+        form_errors_array.push(string);
+        form_errors.value = form_errors_array;
+    }
 });
 
